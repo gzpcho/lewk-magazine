7d1d45fb7d6214a8f829b2aa6e30efa27a48d9f2
gzpcho <roquebertian@gmail.com>
2022-11-17 20:25:49 -0600

    Add GET method to fetch a single article by id

--- api/blueprints/articles.py
+++ api/blueprints/articles.py
@@ -7,3 +7,8 @@ bp = Blueprint("articles", __name__, url_prefix="/api")
 def get_articles():
     article_list = [ a.serialize() for a in Article.query.all() ]
     return make_response(jsonify(article_list), 200)
+
+@bp.route("/article/<article_id>")
+def get_article(article_id):
+    article = Article.query.get_or_404(article_id).serialize()
+    return make_response(jsonify(article), 200)
