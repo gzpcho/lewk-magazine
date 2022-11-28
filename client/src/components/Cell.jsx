97d10b7455df97af080029e9d4011a2873edb762
gzpcho <roquebertian@gmail.com>
2022-11-28 10:18:02 -0600

    Delete unused cell components

--- client/src/components/Cell.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/cell_styles.css';
-function Cell(props){
-    return(
-       <div className='cell'>
-            <div className={props.size}></div>
-        </div>
-    );
-};
-export default Cell;
