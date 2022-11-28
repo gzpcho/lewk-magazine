52d19b60ddb2856013523ebced12261a92597a16
gzpcho <roquebertian@gmail.com>
2022-11-28 02:32:16 -0600

    Add photoshoot upload in client side

---
+++ client/src/services/photoshoot.js
@@ -0,0 +1,39 @@
+import { handleErrors } from './utils.js';
+
+const baseUrl = '/api/photoshoot';
+
+const getAllPhotoshoots = async (params = {}) => {
+  const urlParams = '?' + new URLSearchParams(params).toString();
+  return fetch('/api/photoshoots' + urlParams)
+    .then(handleErrors)
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+};
+
+const getPhotoshoot = async (photoshootId) => {
+  return fetch(`${baseUrl}/${photoshootId}`)
+    .then(handleErrors)
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+};
+
+const postPhotoshoot = async (photoshootId, requestBody) => {
+  return fetch(`${baseUrl}/${photoshootId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+  })
+    .then(handleErrors)
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+};
+
+const photoshootService = {
+  getAll: getAllPhotoshoots,
+  get: getPhotoshoot,
+  post: postPhotoshoot,
+};
+
+export default photoshootService;
