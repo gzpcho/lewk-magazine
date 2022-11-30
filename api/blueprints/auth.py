e07340514d96d5467a1b3ca31cdee8dea4053d9e
gzpcho <roquebertian@gmail.com>
2022-11-29 21:38:32 -0600

    Update auth endpoints to grant/revoke token

--- api/blueprints/auth.py
+++ api/blueprints/auth.py
@@ -1,10 +1,10 @@
 import os
 from flask import Blueprint, make_response, jsonify, request
-from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity
+from flask_jwt_extended import create_access_token, set_access_cookies, unset_jwt_cookies, jwt_required
 
-bp = Blueprint("articles", __name__, url_prefix="/api")
+bp = Blueprint("auth", __name__, url_prefix="/api")
 
-@bp.route("/auth/token", method=["POST"])
+@bp.route("/auth/token", methods=["POST"])
 def create_token():
     try:
         username = request.json["username"]
@@ -15,4 +15,12 @@ def create_token():
     if os.getenv("ADMIN_USERNAME") != username or os.getenv("ADMIN_PASSWORD") != password:
         return make_response(jsonify({"msg": "Wrong credentials."}), 401)
     access_token = create_access_token(identity=username)
-    return make_response(jsonify({"access_token": access_token}), 200)
+    response = jsonify({"access_token": access_token})
+    set_access_cookies(response, access_token)
+    return response
+
+@bp.route('/auth/token/revoke', methods=["POST"])
+def revoke_token():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
